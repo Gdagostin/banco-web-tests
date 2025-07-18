5f6e324dc4e71e3a6a8224d88b285ceb8634067e
Gdagostin <gustavodagostin752@hotmail.com>
2025-07-18 15:45:39 -0300

    Adicionei variáveis de ambiente

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Login', () => {
   beforeEach(() => {
     // Arrange
-    cy.visit('http://localhost:4000/')
+    cy.visit(Cypress.env('URL') || 'http://localhost:3000')
   });
   it('Login com dados válidos deve permitir entrada no sistema', () => {
 
